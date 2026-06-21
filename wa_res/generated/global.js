01c1e89940f91af574dc03be6ebd45affe336e1b
salut023 <nonoguyguy@yahoo.com>
2026-06-21 10:58:27 +0200

    Delete wa_res directory

--- wa_res/generated/global.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", function(){
-wf_init_animation('aos',{once: true});
-});
-WaContext.m_web_messages_developper = {
-'empty sub-folder':'Dossier vide',
-'warning path public folder %1 %2':'Attention : votre site risque de ne pas s’afficher correctement.\nL’emplacement publié ne correspond pas au dossier configuré dans WebAcappella.\nDossier actuel détecté : %1\nDossier configuré dans WebAcappella : %2\nModifiez votre sous-dossier public dans votre configuration de publication et utilisez : %1, puis republiez votre site.\nSi vous êtes un utilisateur avancé et que vous utilisez la réécriture d\'URL (fichier .htaccess), ce message peut être ignoré.\nVous pouvez désactiver cette vérification dans les paramètres de publication de votre site.'
-};
